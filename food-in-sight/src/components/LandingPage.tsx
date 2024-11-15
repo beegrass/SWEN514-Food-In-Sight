de1893675dd572fed2f5e24113c0be377266063b
Lollygagger <ubercharge60@gmail.com>
2024-11-15 15:12:58 -0500

    Added inital compnent for translate:

--- food-in-sight/src/components/LandingPage.tsx
+++ food-in-sight/src/components/LandingPage.tsx
@@ -11,13 +11,18 @@ const LandingPage= () => {
             <main className="main-content">
                 <section className="upload-section">
                     <h2>Upload Food Images</h2>
-                    <input type="file" accept="image/*" />
+                    <input type="file" accept="image/*"/>
                     <button className="upload-button">Upload</button>
                 </section>
                 <section className="search-section">
                     <h2>Search Ingredients</h2>
-                    <input type="text" placeholder="Search for ingredients..." />
+                    <input type="text" placeholder="Search for ingredients..."/>
                     <button className="search-button">Search</button>
+                </section>
+                <section>
+                    <h2>Translate A Label</h2>
+                    <input type="file" accept="image/*"/>
+                    <button className="translate-label">Translate text in image</button>
                 </section>
             </main>
         </div>
